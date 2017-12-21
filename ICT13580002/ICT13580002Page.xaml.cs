1530b03d606df8dec5965d20bd96db127c2905c1
sayrung <32380670+sayrung@users.noreply.github.com>
2017-12-21 13:40:17 +0700

    one

---
+++ ICT13580002/ICT13580002Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13580002
+{
+    public partial class ICT13580002Page : ContentPage
+    {
+        public ICT13580002Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
